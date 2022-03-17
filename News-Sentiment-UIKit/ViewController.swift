15af6f6317660853413b53c10187911b87579837
Alek Michelson <am7590@rit.edu>
2022-03-17 01:20:40 -0400

    Initial Commit

---
+++ News-Sentiment-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News-Sentiment-UIKit
+//
+//  Created by Alek Michelson on 3/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
