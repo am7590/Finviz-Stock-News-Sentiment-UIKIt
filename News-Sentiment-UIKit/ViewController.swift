dff99e3f278379816e7817b0b3ff2582188e113e
Alek Michelson <am7590@rit.edu>
2022-03-17 12:39:26 -0400

    Updated tableview with most recent news

--- News-Sentiment-UIKit/ViewController.swift
+++ News-Sentiment-UIKit/ViewController.swift
@@ -7,18 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     
     var parser = APIParser()
+    var newsArray = [[String]]()
+    var loading = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        // getNews(ticker: "TSLA")
-        getSentiment(ticker: "TSLA")
+        getNews(ticker: "TSLA")
+        // getSentiment(ticker: "TSLA")
     }
     
+    
+    // MARK: Table View Functions
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if loading {
+            return 1
+        } else {
+            return newsArray.count
+        }
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "newsCell", for: indexPath)
+        
+        // Configure cells
+        if loading {
+            cell.textLabel?.text = "Loading news..."
+        } else {
+            let news = newsArray[indexPath.row]
+            cell.textLabel?.text = news[3]
+            cell.detailTextLabel?.text = news[2] + " " + news[1] + " " + news[4]
+            //cell.detailTextLabel?.text = news.
+        }
+        
+        return cell
+    }
+    
+    
+    
+    // MARK: Parse API data
     private func getNews(ticker: String) {
         let component : URLComponents = parser.getNewsRequest(ticker: ticker)
         // var returnJSON : [String : Int] = [:]
@@ -35,7 +67,11 @@ class ViewController: UIViewController {
                 do {
                     let parsedJSON = try jsonDecoder.decode(NewsStruct.self, from: data)
                     
-                    print(parsedJSON)
+                    print(parsedJSON.content.description)
+                    
+                    
+                    
+                    self.newsArray = parsedJSON.content
                     
                     //returnJSON = self.parser.sortData(parsedJSON: parsedJSON)
                     
@@ -48,6 +84,13 @@ class ViewController: UIViewController {
                     print("Error parsing JSON")
                 }
             }
+            
+            self.loading = false
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            
             
         }).resume()
     }
